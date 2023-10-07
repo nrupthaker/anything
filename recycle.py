964c4a79b5da24bf94e297c178c827d7addd8ab1
nrupthaker <147276052+nrupthaker@users.noreply.github.com>
2023-10-07 13:30:55 -0700

    Update recycle.py

--- recycle.py
+++ recycle.py
@@ -1,1 +1,14 @@
 
+import streamlit as st
+
+# Title
+st.title("Simple Streamlit App")
+
+# Text Input
+user_input = st.text_input("Enter some text:")
+button_clicked = st.button("Click me!")
+
+# Display entered text when button is clicked
+if button_clicked:
+    st.write("You entered:", user_input)
+
